eda42c2fd7d9aec327c7106aeb1cab99c3cb49d1
HAPM06-PC14\SV <mybui11qwer@gmail.com>
2024-09-26 15:48:10 +0700

    Updatee 26/9

---
+++ 22dh112218-Bui Khanh My/Lab02/Lab02/Lab02/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections;
+using Lab02.Models;
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            DataModel db = new DataModel();
+            ViewBag.list = db.get("SELECT * FROM SACH");
+            return View();
+        }
+
+        public ActionResult SanPham()
+        {
+            DataModel db = new DataModel();
+            ViewBag.list = db.get("EXEC xuatdulieu");
+            return View();
+        }
+        public ActionResult ChuDe()
+        {
+            DataModel db = new DataModel();
+            ViewBag.list = db.get("SELECT * FROM CHUDE");
+            return View();
+        }
+        public ActionResult NhaXuatBan()
+        {
+            DataModel db = new DataModel();
+            ViewBag.list = db.get("SELECT * FROM NHAXUATBAN");
+            return View();
+        }
+        public ActionResult Tacgia()
+        {
+            DataModel db = new DataModel();
+            ViewBag.list = db.get("SELECT * FROM TACGIA");
+            return View();
+        }
+        public ActionResult Vietsach()
+        {
+            DataModel db = new DataModel();
+            ViewBag.list = db.get("SELECT * FROM VIETSACH");
+            return View();
+        }
+        public ActionResult DonDatHang()
+        {
+            DataModel db = new DataModel();
+            ViewBag.list = db.get("SELECT * FROM DONDATHANG");
+            return View();
+        }
+        public ActionResult CTDatHang()
+        {
+            DataModel db = new DataModel();
+            ViewBag.list = db.get("SELECT * FROM CTDATHANG");
+            return View();
+        }
+
+        public ActionResult KhachHang()
+        {
+            DataModel db = new DataModel();
+            ViewBag.list = db.get("SELECT * FROM KHACHHANG");
+            ViewBag.listcd = db.get("SELECT * FROM CHUDE");
+            return View();
+        }
+        public ActionResult TimKiemSach(string tensach)
+        {
+            DataModel db = new DataModel();
+            ViewBag.list = db.get("EXEC TIMKIEMSACHTHEOTEN '" + tensach + "'");
+            return View();
+        }
+        public ActionResult ChiTietSach(string id)
+        {
+            DataModel db=new DataModel();
+            ViewBag.list = db.get("EXEC TIMKIEMSACHTHEOID " + id +";");
+            return View();
+        }
+
+    }
+}
+
---
+++ 22dh112218-Bui Khanh My/Lab02/Lab02/Lab02/Models/DataModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections; // Sử dụng Lớp ArrayList để lưu kết quả
+using System.Data.SqlClient;// Sử dụng các lớp tương tác CSDL
+namespace Lab02.Models
+{
+    public class DataModel
+    {
+        public static string connectionString = "workstation id=demodoanpmlab02.mssql.somee.com;packet size=4096;user id=oblivion2504_SQLLogin_3;pwd=58spdpquvw;data source=demodoanpmlab02.mssql.somee.com;persist security info=False;initial catalog=demodoanpmlab02;TrustServerCertificate=True";
+        public ArrayList get(String sql)
+        {
+            ArrayList datalist = new ArrayList();
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            connection.Open();
+
+            using (SqlDataReader r = command.ExecuteReader())
+            {
+                while (r.Read())
+                {
+                    ArrayList row = new ArrayList();
+                    for (int i = 0; i < r.FieldCount; i++)
+                    {
+                        row.Add(r.GetValue(i).ToString());
+                    }
+                    datalist.Add(row);
+                }
+            }
+            connection.Close();
+            return datalist;
+        }
+    }
+}
